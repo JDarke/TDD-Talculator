1096364d140fcb0a4bf04e8cdeb47c8ad64d23e1
John Darke <johndarke@live.co.uk>
2020-08-24 09:56:13 +0100

    build

---
+++ build/precache-manifest.1ba98fbc092bdfa90fadde01825dd8f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28824c44145af7783a0aa084677dae5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64d21f0604c5df49dc63",
+    "url": "/static/css/main.7b108d1a.chunk.css"
+  },
+  {
+    "revision": "c5a070430da03b2908bd",
+    "url": "/static/js/2.c87c1534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c87c1534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d21f0604c5df49dc63",
+    "url": "/static/js/main.ecd6a283.chunk.js"
+  },
+  {
+    "revision": "49eb44dfb8a4b252d9d4",
+    "url": "/static/js/runtime-main.bd74e7d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
